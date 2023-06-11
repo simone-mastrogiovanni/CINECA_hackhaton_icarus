2906e4617283872911b78df555039a6c9d9d80b2
Simone Mastrogiovanni <smastrog@login03.m100.cineca.it>
2023-06-11 20:21:42 +0200

    added timing results

--- geticarogw_jax.sh
+++ geticarogw_jax.sh
@@ -1,4 +1,5 @@
 conda env create -f environment_jax.yml --name icarogwjax
 conda activate icarogwjax
+pip install --upgrade pip
 pip install git+https://github.com/simone-mastrogiovanni/icarogw.git@jax_comparison
 pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
--- profiling_GPU.py
+++ profiling_GPU.py
@@ -6,13 +6,13 @@ from astropy.cosmology import FlatLambdaCDM
 import time
 from tqdm import tqdm
 import sys
+from icarogw.cupy_pal import *
 
-
-
-
-Nev = 500
+Ntot = [1e5,5e5,1e6,5e6,1e7]
+Nsamples = 10000
 Nrep = 100
-timing_flag = True
+Ninj = 10000
+timing_flag = 'timing/GPU.txt'
 
 # Again we need to tell icarogw what reference cosmology was used to build the catalog
 cosmo_ref=icarogw.cosmology.astropycosmology(zmax=10.)
@@ -28,19 +28,20 @@ data=h5py.File('endo3_nsbhpop-LIGO-T2100113-v12.hdf5')
 # We select the maximum IFAR among the searches
 ifarmax=np.vstack([data['injections'][key] for key in ['ifar_cwb', 'ifar_gstlal', 'ifar_mbta', 'ifar_pycbc_bbh', 'ifar_pycbc_hyperbank']])
 ifarmax=np.max(ifarmax,axis=0)
+
 time_O3 = (28519200/86400)/365 # Time of observation for O3 in tr
 # The prior for this injection set is saved in source frame
 prior=data['injections/mass1_source_mass2_source_sampling_pdf'][()]*data['injections/redshift_sampling_pdf'][()]/(np.pi*4) # Add prior on sky angle (isotropic)
-prior*=icarogw.conversions.source2detector_jacobian(data['injections/redshift'][()],cosmo_ref) # Add jacobian to convert prior in detector frame
+prior*=cp2np(icarogw.conversions.source2detector_jacobian(np2cp(data['injections/redshift'][()]),cosmo_ref)) # Add jacobian to convert prior in detector frame
 # Prepare the input data
-injections_dict={'mass_1':data['injections/mass1'][()],'mass_2':data['injections/mass2'][()],
-                'luminosity_distance':data['injections/distance'][()],
-                'right_ascension':data['injections/right_ascension'][()],'declination':data['injections/declination'][()]}
+injections_dict={'mass_1':data['injections/mass1'][()][:Ninj],'mass_2':data['injections/mass2'][()][:Ninj],
+                'luminosity_distance':data['injections/distance'][()][:Ninj],
+                'right_ascension':data['injections/right_ascension'][()][:Ninj],'declination':data['injections/declination'][()][:Ninj]}
 # Initialize the injections as usual
-inj=icarogw.injections.injections(injections_dict,prior=prior,ntotal=data.attrs['total_generated'],Tobs=time_O3)
+inj=icarogw.injections.injections(injections_dict,prior=prior[:Ninj],ntotal=data.attrs['total_generated'],Tobs=time_O3)
 # Select injections with IFAR higher than 4yr
 inj.cupyfy()
-inj.update_cut(ifarmax>=4)
+inj.update_cut(ifarmax[:Ninj]>=4)
 # Pixelize the injections
 inj.pixelize(nside=nside)
 
@@ -55,33 +56,39 @@ rate_model = icarogw.wrappers.CBC_catalog_vanilla_rate(cat,cosmo_wrap,
                                                        #detection probability to evaluate selection biases
                                                        scale_free=True)
 
-posterior_dict={}
-for i in range(Nev):
-    GW190814=h5py.File('GW190814_pubsamp.hdf5') # We load the PE release for GW190814
+if timing_flag is not None:
+    fp=open(timing_flag,'w')
 
-    # We extract all the data we need for GW190814
-    ppd={'mass_1':GW190814['PE/mass_1'][:], # m1 in detector frame
-        'mass_2':GW190814['PE/mass_2'][:], # m2 in detector frame
-        'luminosity_distance':GW190814['PE/luminosity_distance'][:], # Luminosity distance in Mpc
-        'right_ascension':GW190814['PE/right_ascension'][:], # right ascension in radians
-        'declination':GW190814['PE/declination'][:]} # Declination in radians
-    posterior_dict['GW'+str(i)]=icarogw.posterior_samples.posterior_samples(ppd,
-                                                                             prior=np.power(GW190814['PE/luminosity_distance'][:],2.))
-    posterior_dict['GW'+str(i)].pixelize(nside)
-    posterior_dict['GW'+str(i)].cupyfy()
-posterior_dict = icarogw.posterior_samples.posterior_samples_catalog(posterior_dict)
+for nt in Ntot:
+    Nev=int((nt-Ninj)/Nsamples)
+    posterior_dict={}
+    for i in range(Nev):
+        GW190814=h5py.File('GW190814_pubsamp.hdf5') # We load the PE release for GW190814
+        # We extract all the data we need for GW190814
+        ppd={'mass_1':GW190814['PE/mass_1'][:Nsamples], # m1 in detector frame
+          'mass_2':GW190814['PE/mass_2'][:Nsamples], # m2 in detector frame
+         'luminosity_distance':GW190814['PE/luminosity_distance'][:Nsamples], # Luminosity distance in Mpc
+         'right_ascension':GW190814['PE/right_ascension'][:Nsamples], # right ascension in radians
+         'declination':GW190814['PE/declination'][:Nsamples]} # Declination in radians
+        posterior_dict['GW'+str(i)]=icarogw.posterior_samples.posterior_samples(ppd,
+                                                                             prior=np.power(GW190814['PE/luminosity_distance'][:Nsamples],2.))
+        posterior_dict['GW'+str(i)].pixelize(nside)
+        posterior_dict['GW'+str(i)].cupyfy()
+    posterior_dict = icarogw.posterior_samples.posterior_samples_catalog(posterior_dict)
 
-# Likelihood definition
-likelihood=icarogw.likelihood.hierarchical_likelihood(posterior_dict,inj,rate_model,nparallel=5096,neffINJ=None,neffPE=20)
+    # Likelihood definition
+    likelihood=icarogw.likelihood.hierarchical_likelihood(posterior_dict,inj,rate_model,nparallel=Nsamples,neffINJ=-1,neffPE=-1)
 
-timing=np.zeros(Nrep)
-for i in tqdm(range(Nrep)):
-    likelihood.parameters={'Om0':0.308,'alpha':3.78,'beta':0.81,'mmin':4.98,'mmax':112.5,'delta_m':4.8,'mu_g':32.27,'sigma_g':3.88,
-                          'lambda_peak':0.03,'gamma':4.59,'kappa':2.86,'zp':2.47,'mmin_NS':1.,'mmax_NS':3.,'delta_m_NS':0.,'H0':67.7}
-    start=time.time()
-    _=likelihood.log_likelihood()
-    end=time.time()
-    timing[i]=end-start
+    timing=np.zeros(Nrep)
+    for i in tqdm(range(Nrep)):
+        likelihood.parameters={'Om0':0.308,'alpha':3.78,'beta':0.81,'mmin':4.98,'mmax':112.5,'delta_m':4.8,'mu_g':32.27,'sigma_g':3.88,
+                              'lambda_peak':0.03,'gamma':4.59,'kappa':2.86,'zp':2.47,'mmin_NS':1.,'mmax_NS':3.,'delta_m_NS':0.,'H0':67.7}
+        start=time.time()
+        _=likelihood.log_likelihood()
+        end=time.time()
+        timing[i]=end-start
 
-if timing_flag:
-    print('The time is {:f} pm {:f}'.format(np.mean(timing),np.std(timing)))
+    if timing_flag:
+        fp.write('{:e}\t{:e}\t{:e}\n'.format(nt,np.median(timing),np.std(timing)))
+
+fp.close()
